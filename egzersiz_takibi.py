84460e6a20816fe56a33dc85bfe02bf585db1db2
merbeck <asbadur01@gmail.com>
2025-05-26 20:04:22 +0300

    Add files via upload

---
+++ egzersiz_takibi.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title('🏋️ Egzersiz Takibi')
+st.write('Egzersiz geçmişi ve giriş alanı.')
---
+++ olcum_takibi.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title('📏 Vücut Ölçüm Takibi')
+st.write('Boy, kilo, yağ oranı vb.')
